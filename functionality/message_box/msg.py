b74f2751fd3f22551e155ce9b1b1331f6c7925fb
Kadabrak <139461430+Kadabrak@users.noreply.github.com>
2023-07-15 19:50:23 +0200

    Update msg.py

--- functionality/message_box/msg.py
+++ functionality/message_box/msg.py
@@ -2,11 +2,11 @@ import os
 
 def get_msg(name_reciver):
     path = os.getcwd()
-    box_path = path+'\\functionality\\message_box'
+    box_path = path+'/functionality/message_box'
     file_in_path = os.listdir(box_path)
     for i in file_in_path:
         if i[0:-4] == name_reciver:
-            fich_message = box_path+'\\'+i
+            fich_message = box_path+'/'+i
             with open(fich_message,'r') as fich:
                 fich_content = fich.readlines()
             os.remove(fich_message)
@@ -16,9 +16,9 @@ def get_msg(name_reciver):
 
 def place_msg(sender,reciver,message):
     path = os.getcwd()
-    box_path = path+'\\functionality\\message_box'
+    box_path = path+'/functionality/message_box'
     try:
-        with open((box_path+'\\'+reciver+'.txt'),'a') as fich:
+        with open((box_path+'/'+reciver+'.txt'),'a') as fich:
             fich.write(('from '+ sender +' ' + ' '.join(message) +'\n'))
             return True
     except:
